bcdfbc48781e5c511febff5ff06bc8eb29d89cd5
Noisekit <noisekit@proton.me>
2025-03-07 11:32:12 +1100

    Coming soon

--- staking/ui/src/Staking/EmptyPosition.tsx
+++ staking/ui/src/Staking/EmptyPosition.tsx
@@ -11,18 +11,25 @@ export function EmptyPosition() {
     <LayoutWithImage
       imageSrc={coin}
       Subheader={() => (
-        <Text
-          color="gray.50"
-          fontSize={['30px', '36px']}
-          fontWeight={500}
-          lineHeight={1.25}
-          letterSpacing="tight"
-        >
-          Debt-free staking coming soon.
-        </Text>
+        <>
+          <Text
+            color="gray.50"
+            fontSize={['30px', '36px']}
+            fontWeight={500}
+            lineHeight={1.25}
+            letterSpacing="tight"
+          >
+            Debt-free staking coming soon.
+          </Text>
+        </>
       )}
       Content={() => (
         <>
+          <Text color="gray.600">
+            The 420 Pool is currently accepting deposits from active stakers. Get SNX now to prepare
+            for debt-free SNX yield.
+          </Text>
+
           <ButtonStake />
 
           <Flex gap={4} justifyContent="space-between">
